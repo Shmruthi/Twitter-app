ee406e8e492a7d24e92b60eaa2368e88ab721ff9
Shmruthi <shmruthi@gmail.com>
2011-06-03 19:34:35 +0530

    End of 3-6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   def new
     @title="Signup"
+    @user= User.new
   end
   def show
     @user = User.find(params[:id])
+  end
+  def create
+    @user= User.new
+    render 'new'
+
   end
 end
