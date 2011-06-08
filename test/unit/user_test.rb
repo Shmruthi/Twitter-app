7f52f6d6a81c0552cea665bd6760b9aa9a6b85f1
Shmruthi <shmruthi@gmail.com>
2011-06-08 17:33:20 +0530

    initial

---
+++ test/unit/user_test.rb
@@ -0,0 +1,11 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require 'test_helper'
+
+class PostTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
